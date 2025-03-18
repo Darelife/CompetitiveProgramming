1d7c045b7ba4f1ca8c5c1ba17778ebe4c1d27767
Prakhar Bhandari <prakharb2k6@gmail.com>
2025-03-18 21:56:01 +0530

    been a while

--- Problems/B_Preparing_for_the_ContestSave_to_List.cpp
+++ Problems/B_Preparing_for_the_ContestSave_to_List.cpp
@@ -1,5 +1,3 @@
-// heavily inspired from sankabapur's code btw (he explained it to me)
-
 #include <bits/stdc++.h>
 #include <ext/pb_ds/assoc_container.hpp>
 #include <ext/pb_ds/tree_policy.hpp>
@@ -22,90 +20,89 @@ const int inf = 1e9 + 5;
 const int MOD = 1e9 + 7;
 
 int check(vector<pair<int, int>>& a, vector<tuple<int, int, int>>& v, int mid, vint& res, int s) {
-  int m = a.size(), n = v.size(), cost = 0, nextInPq = 0, curStud = -1;
+  int m = a.size(), n = v.size(), cost = 0, remainingEle = 0, studGrind = -1;
   vint cnt(n, 0);
   priority_queue<tuple<int, int, int>, vector<tuple<int, int, int>>, greater<tuple<int, int, int>>> pq;
-  f(i, 0, m) {
-    int l = 0, r = n - 1, index = -1;
+
+  for (int i = 0; i < m; i++) {
+
+    int l = 0, r = n - 1, idx = -1;
     while (l <= r) {
       int mid = l + (r - l) / 2;
       if (get<0>(v[mid]) >= a[i].first) {
-        index = mid;
+        idx = mid;
         l = mid + 1;
       } else {
         r = mid - 1;
       }
     }
-    for (int j = nextInPq; j <= index && j < n; j++) {
-      pq.push({ get<1>(v[j]), get<0>(v[j]), get<2>(v[j]) });
-    }
-    nextInPq = index + 1;
 
-    if (pq.empty() && curStud == -1) {
-      res.resize(m, -1);
-      return -1;
-    }
-    if (curStud != -1) {
-      cnt[curStud]++;
-      res[a[i].second] = curStud;
-      if (cnt[curStud] == mid) curStud = -1;
+    for (int j = remainingEle; j <= idx; j++)
+      pq.push({ get<1>(v[j]), get<0>(v[j]), get<2>(v[j]) });
+    remainingEle = idx + 1;
+
+    if (pq.empty() && studGrind == -1) return -1;
+
+    if (studGrind != -1) {
+      res[a[i].second] = studGrind;
+      cnt[studGrind]++;
+      if (cnt[studGrind] == mid) studGrind = -1;
     } else {
-      tuple<int, int, int> t = pq.top();
+      tuple<int, int, int> ele = pq.top();
       pq.pop();
-      cost += get<0>(t);
-      int index = get<2>(t);
-      cnt[index]++;
-      res[a[i].second] = index;
-      curStud = index;
-      if (cnt[index] == mid) curStud = -1;
+      cost += get<0>(ele);
+      res[a[i].second] = get<2>(ele);
+      cnt[get<2>(ele)]++;
+      studGrind = get<2>(ele);
+      if (cnt[get<2>(ele)] == mid) studGrind = -1;
     }
   }
+
   if (cost > s) return -1;
-  return cost;
+  else return cost;
 }
 
 void solve() {
   int n, m, s;
   cin >> n >> m >> s;
+
   vint res(m, -1);
   vector<pair<int, int>> a(m);
-  f(i, 0, m) {
-    cin >> a[i].first;
-    a[i].second = i;
-  }
-  vint b(n);
-  vcin(b, n);
-  vint c(n);
-  vcin(c, n);
+  f(i, 0, m) cin >> a[i].first;
+  f(i, 0, m) a[i].second = i;
+
+
   vector<tuple<int, int, int>> v(n);
-  f(i, 0, n) {
-    v[i] = { b[i], c[i], i };
-  }
+  f(i, 0, n) cin >> get<0>(v[i]);
+  f(i, 0, n) cin >> get<1>(v[i]);
+  f(i, 0, n) get<2>(v[i]) = i;
+
   sort(a.rbegin(), a.rend());
   sort(v.rbegin(), v.rend());
 
-  int l = 1, r = 1e5;
   vint ans(m, -1);
   int cost = -1;
 
+  int l = 1, r = m;
   while (l <= r) {
     int mid = l + (r - l) / 2;
     res.resize(m, -1);
-    int c = check(a, v, mid, res, s);
-    if (c == -1) {
+    int t = check(a, v, mid, res, s);
+    if (t == -1) {
       l = mid + 1;
     } else {
       r = mid - 1;
-      cost = c;
+      cost = t;
       ans = res;
     }
   }
-  if (cost == -1) {
-    cout << "NO\n";
-  } else {
+
+  if (cost != -1) {
     for (int i = 0; i < m; i++) ans[i]++;
     cout << "YES\n";
     vpin(ans);
+  } else {
+    cout << "NO\n";
   }
 }
 
