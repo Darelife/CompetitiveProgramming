8ca6c12e0caae57ec5150cbf2188aa94e520e6ba
darelife <prakharb2k6@gmail.com>
2025-05-20 09:12:45 +0530

    sip prep D_yet another problem on a subsequence codeforces

--- Problems/D_Yet_Another_Problem_On_a_Subsequence.cpp
+++ Problems/D_Yet_Another_Problem_On_a_Subsequence.cpp
@@ -28,7 +28,39 @@ void solve() {
   vector<vector<int>> dp(n + 1, vector<int>(n + 1, 0));
   /*
     * dp[i][rem] = number of good subsequences from i to n-1
+    * such that we'll need to add "rem" elements to make it good
   */
+
+  // *base case : for the last element, if  rem == 0, it's 1, otherwise, it's 0
+  dp[n][0] = 1;
+
+  /*
+    * Transition
+    * if picked, dp[i][rem] = (rem==0)?(dp[i+1][a[i]]):(dp[i+1][rem-1])
+    * else, dp[i][rem] = dp[i+1][rem]
+  */
+
+  int mod = 998244353;
+
+  for (int i = n - 1; i >= 0; i--) {
+    for (int rem = 0; rem <= n; rem++) {
+      int ans = 0;
+      // * Case 1: We skip the element
+      ans += (dp[i + 1][rem]) % mod;
+
+      if (rem == 0) {
+        // * The condition they gave us
+        // * a[i] > 0 && also, a[i] <= n for the other condition
+        if (a[i] > 0 && a[i] <= n) ans = (ans + dp[i + 1][a[i]]) % mod;
+      } else {
+        ans = (ans + dp[i + 1][rem - 1]) % mod;
+      }
+      dp[i][rem] = ans;
+    }
+  }
+
+  // * Subtracting 1 to remove the case where no element got selected
+  cout << (dp[0][0] - 1 + mod) % mod << endl;
 }
 
 int32_t main() {
